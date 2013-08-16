b238e56afbd8442854b3c547055fd2ac9f76edfe
jpineda94 <joel.pineda@unitec.edu>
2013-08-16 16:16:24 -0600

    Agregando Comentarios

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,28 +8,36 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    //Creación de una nueva ventana
     ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
+    //Libera de la memoria la ventana principal
     delete ui;
 }
 
 void MainWindow::on_actionNuevo_triggered()
 {
+    //Guarda la direccion de la direccion del archivo que se crea
     QString filename = QFileDialog::getSaveFileName(this,"New File",QDir::homePath(),"TextFile(*.txt)");
 }
 
 void MainWindow::on_actionCrear_triggered()
 {
+    //Se hace un apuntador de una nueva ventana VentanaCrearCampos para poder manipularla
     VentanaCrearCampos* crearCampos = new VentanaCrearCampos();
 
+    //Se ejecuta la ventana
     crearCampos->exec();
+
+    //Cuando termina de ejecutarse, se destruye la ventana
     delete crearCampos;
 }
 
 void MainWindow::on_actionSalir_triggered()
 {
+    //Cierra la ventana principal
     this->close();
 }
--- ventanacrearcampos.cpp
+++ ventanacrearcampos.cpp
@@ -5,15 +5,18 @@ VentanaCrearCampos::VentanaCrearCampos(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::VentanaCrearCampos)
 {
+    //Creacion de una nueva ventana
     ui->setupUi(this);
 }
 
 VentanaCrearCampos::~VentanaCrearCampos()
 {
+    //Libera de la memoria la ventana que se destruye
     delete ui;
 }
 
 void VentanaCrearCampos::on_btCancelarNuevoCampo_clicked()
 {
+    //Cierra la ventana principal
     this->close();
 }
